4140ff457168cbbc762772ca1f1be82e1bf6fd59
Benni <>
2021-10-01 15:05:44 +0200

    funktioniert grob

--- terminal.py
+++ terminal.py
@@ -16,13 +16,37 @@ except:
     print("COM Port nicht erreichbar, du Nugget!")
     exit()
 
-new_line = conf[5]
+if conf[5].strip() == "\r":
+    new_line = "\r"
+elif conf[5].strip() == "\n":
+    new_line = "\r\n"
+elif conf[5] .strip() == "\r\n":
+    new_line = "\r\n"
+else:
+    new_line = ""
 
 print("starte Terminal")
 
+user_key = ""
+user_str = ""
+serial_line = ""
+
 #super loop
 while True:
-    pass
     #serial readline print
+    if com.inWaiting:
+        serial_line = com.readline().decode("iso-8859-1")
+        if serial_line != "":
+            print(serial_line)
     #wenn benutzer taste gedrückt blockieren bis enter dann printen
-    #wenn "-q" einegeben com port schließen
+    if msvcrt.kbhit():
+        user_key = msvcrt.getch().decode()
+        if user_key == "\r":
+            com.write((user_str + new_line).encode())
+            print("User> " + user_str)
+            user_key = ""
+            #wenn "-q" einegeben com port schließen
+            if user_str == "-q":
+                break
+            user_str = ""
+        user_str += user_key
