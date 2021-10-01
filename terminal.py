edbe7425f204fd2ba6e4d696b79af091f1b887da
Benni <>
2021-10-01 14:35:16 +0200

    terminal.py angelegt

---
+++ terminal.py
@@ -0,0 +1,11 @@
+#config.txt einlesen
+
+#com port öffnen
+
+#super loop
+
+#serial readline print
+
+#wenn benutzer taste gedrückt blockieren bis enter dann printen
+
+#wenn "-q" einegeben com port schließen
