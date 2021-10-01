96b5225eddca5987deafdb670a53b33c1f58f477
Benni <>
2021-10-01 14:47:24 +0200

    super loop

--- terminal.py
+++ terminal.py
@@ -8,18 +8,21 @@ conf = conf_handler.readlines()
 #com port öffnen
 com = Serial()
 com.timeout = 0.2
-com.port = "COM" + str(conf[2].strip())
-com.baudrate = int(conf[4].strip())
+com.port = "COM" + str(conf[1].strip())
+com.baudrate = int(conf[3].strip())
 try:
     com.open()
 except:
     print("COM Port nicht erreichbar, du Nugget!")
     exit()
 
+new_line = conf[5]
+
+print("starte Terminal")
+
 #super loop
-
-#serial readline print
-
-#wenn benutzer taste gedrückt blockieren bis enter dann printen
-
-#wenn "-q" einegeben com port schließen
+while True:
+    pass
+    #serial readline print
+    #wenn benutzer taste gedrückt blockieren bis enter dann printen
+    #wenn "-q" einegeben com port schließen
